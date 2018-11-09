1b5964df9264bcef7f074f261f58078c9c66d9fb
cn <cn@mycrobase.de>
2018-11-09 14:08:59 +0100

    gem: pin seven_zip < 3.0 manually before build-essential to retain Chef 12 support

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      "Installs dyndnsd.rb as gem and configures it."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.3.0"
 
+depends "seven_zip", "< 3.0.0" # pin manually before build-essential to retain Chef 12 support
 depends "build-essential"
 
 suggests "logrotate" # for dyndnsd::logrotate
